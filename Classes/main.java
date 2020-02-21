ddaef7d40169f1442f2ea24d2db5bc1a539a23db
unknown <moaz.eldefrawy@gmail.com>
2020-02-21 19:56:45 +0100

    whatever

---
+++ Classes/main.java
@@ -0,0 +1,18 @@
+package Calculators.Classes;
+
+import Calculators.interfaces.ICalculator;
+
+public class main implements ICalculator {
+
+
+    public int add(int x, int y) {
+        return x+y;
+    }
+
+    public float divide(int x, int y) throws RuntimeException {
+        if(y == 0)
+            throw new RuntimeException("Can\'t divide by 0");
+        return (float)x/y;
+
+    }
+}
